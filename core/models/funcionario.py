18d3ab279befab53cd7938ef59f80120fe2a3f53
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-08-12 15:41:27 -0300

    FEAT: adcionar models de user, funcionario, paciente e admin  #15

---
+++ core/models/funcionario.py
@@ -0,0 +1,13 @@
+from django.db import models
+from core.models import UsuarioBase, Clinica
+
+class Profissional(models.Model):
+    usuario = models.OneToOneField(UsuarioBase, on_delete=models.PROTECT)
+    cpf = models.CharField(max_length=11, unique=True)
+    matricula = models.CharField(max_length=300)
+    telefone = models.CharField(max_length=20)
+    data_nascimento = models.DateField()
+    clinica = models.ForeignKey(Clinica, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.usuario.username
---
+++ core/models/paciente.py
@@ -0,0 +1,17 @@
+from django.db import models
+from core.models import UsuarioBase, Clinica
+
+class Paciente(models.Model):
+    usuario = models.OneToOneField(UsuarioBase, on_delete=models.PROTECT)
+    cpf = models.CharField(max_length=11, unique=True)
+    endereco = models.CharField(max_length=150)
+    numero = models.IntegerField()
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=150)
+    cep = models.CharField(max_length=10)
+    complemento = models.CharField(max_length=200, blank=True, null=True)
+    data_nascimento = models.DateField()
+    clinica = models.ForeignKey(Clinica, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.usuario.username
