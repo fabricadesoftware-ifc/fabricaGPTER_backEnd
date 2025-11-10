de351f80a93cbce4ee3e439642d34f24fee48c2f
erickChavesFiamoncini <erickechaves@gmail.com>
2025-11-10 10:40:30 -0300

    FEAT: atualizar registro do modelo Teste no admin; corrigir versão do Django nas dependências

--- core/admin.py
+++ core/admin.py
@@ -4,8 +4,10 @@ from core.models.paciente import Paciente
 from core.models.admin import Administrador
 from core.models.profissional import Profissional
 from core.models.teste import Teste
+from core.models.user import UsuarioBase
 
 admin.site.register(Paciente)
 admin.site.register(Administrador)
 admin.site.register(Profissional)
 admin.site.register(Teste)
+admin.site.register(UsuarioBase)
